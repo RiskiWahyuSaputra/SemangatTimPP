39cc33af39d1f924da1d9eaa4e0431e212177b39
RiskiWahyuSaputra <arskies2005@gmail.com>
2025-11-18 16:08:09 +0700

    menambahkan button registrasi

--- index.php
+++ index.php
@@ -92,6 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <input type="password" id="password" name="password" required>
             </div>
             <button type="submit" class="btn-login">Login</button>
+            <button type="submit" class="btn-login">Registrasi</button>
         </form>
     </div>
 </body>
